3c54701db4d1a541e56ee0cfcefc4a54b30d7c1f
Ian Vlahovic <banddorkfht7@gmail.com>
2018-03-20 20:53:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
 
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -17,10 +18,6 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@songs
